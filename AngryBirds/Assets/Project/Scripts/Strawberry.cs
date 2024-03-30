a0565159b3a942ef84deeba9ad061ab782020b92
heyyjoud <ja0139@mix.wvu.edu>
2024-03-30 19:07:04 -0400

    added a somewhat functioning counter and tried to stop the strawberries from generating more than once

--- AngryBirds/Assets/Project/Scripts/Strawberry.cs
+++ AngryBirds/Assets/Project/Scripts/Strawberry.cs
@@ -13,6 +13,8 @@ public class Strawberry : MonoBehaviour
     Rigidbody2D rb;
     bool hasHit; 
 
+    private static int chances = 0;
+
     void Start() {
 
         rb = GetComponent<Rigidbody2D>();
@@ -26,59 +28,69 @@ public class Strawberry : MonoBehaviour
 
     private void Update()
     {
-
+    
         GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
         GetComponent<LineRenderer>().SetPosition(0, transform.position);
 
-        if (_birdWasLaunched &&
-            GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
-        {
-            _timeSittingAround += Time.deltaTime;
-        }
+        if (chances < 2) {
 
+            if (_birdWasLaunched &&
+                GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+            {
+                _timeSittingAround += Time.deltaTime;
+                
+            }
 
-        if (transform.position.y > 10 ||
+            //set to 2 because the scene reinstantiates an additional time
+            
+            if (transform.position.y > 10 ||
             transform.position.y < -10 ||
             transform.position.x > 10 ||
             transform.position.x < -10 ||
             _timeSittingAround > 3)
-        {
-            string currentSceneName = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene(currentSceneName);
-        }
+            {
+                string currentSceneName = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(currentSceneName);
+                chances++;
+                Debug.Log("You used " + chances + " chances");
+                
+            }
 
+            
+        }
         
     }
 
    private void OnMouseDown()
    {
-    // GetComponent<SpriteRenderer>().color = Color.red;
-    GetComponent<LineRenderer>().enabled = true;
+        // GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
    }
 
    private void OnMouseUp()
    {
-    GetComponent<SpriteRenderer>().color = Color.white;
+        GetComponent<SpriteRenderer>().color = Color.white;
 
-    Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
 
-    GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
-    GetComponent<Rigidbody2D>().gravityScale = 1;
-    _birdWasLaunched = true;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLaunched = true;
 
-    GetComponent<LineRenderer>().enabled = false;
+        GetComponent<LineRenderer>().enabled = false;
    }
 
    private void OnMouseDrag()
    {
-    Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    transform.position = new Vector3(newPosition.x, newPosition.y);
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
    }
 
     // to make the strawberry stick upon collision
    private void OnCollisionEnter2D(Collision2D collision) {
-        // hasHit = true;
+
         rb.velocity = Vector2.zero;
         rb.isKinematic = true;
    }
+
 }
